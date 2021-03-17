49c519c068b32226b098e864dcbb502daf4eaf4c
herzjulius <48582371+herzjulius@users.noreply.github.com>
2021-03-18 00:21:27 +0100

    Add files via upload

---
+++ LittlePongApp/GameBall.swift
@@ -0,0 +1,45 @@
+//
+//  GameBall.swift
+//  LittlePongApp
+//
+//  Created by Julius Herz on 17.03.21.
+// changes
+
+import Foundation
+import SpriteKit
+import GameplayKit
+
+
+//creates GameBall Class
+class GameBall : SKShapeNode{
+    override init() {
+        super.init()
+    }
+
+    convenience init(radius: CGFloat, velo: CGVector, color: UIColor, name: String ) {
+        self.init(circleOfRadius: radius)
+        self.fillColor = color
+        self.physicsBody = SKPhysicsBody(circleOfRadius: radius)
+        self.physicsBody?.velocity = velo
+        self.physicsBody?.affectedByGravity = false
+        self.physicsBody?.isDynamic = true
+        self.physicsBody!.allowsRotation = false
+        self.physicsBody?.linearDamping = 0
+        self.physicsBody?.friction = 0
+        self.physicsBody!.restitution = 1
+        self.name = name
+        // Do stuff
+     }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+        
+        
+        
+    }
+
+
+    
+}
+
+
